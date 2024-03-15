867f7f37431062c49810fcee53725608b6d179d2
GVL0001 <gustavovl0001@gmail.com>
2024-03-14 21:37:28 -0500

    Pruebas automatizadas version final

--- configuration.py
+++ configuration.py
@@ -1,8 +1,11 @@
 # Constante: url del servidor
-URL_SERVICE = "https://cnt-1ca35206-6e76-4dc4-9f14-2bd4e14802f7.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-09b7aeb8-1a36-4b86-b773-16ad5b1ea558.containerhub.tripleten-services.com"
 
 # Constante: endpoint para crear un nuevo usuario
 CREATE_USER_PATH = "/api/v1/users/"
 
 # Constante: endpoint para crear un kit
 KITS_PATH = "/api/v1/kits/"
+
+# Constante: endpoint para recuperar información de la tabla de base de datos
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,11 +2,13 @@ import requests
 import configuration
 
 
+# Función para la creación de un nuevo usuario
 def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
                          json=body)
 
 
+# Función para la creación de un nuevo kit de usuario
 def post_new_user_kit(kit_body, auth_token):
     headers = {
         "Authorization": f"Bearer {auth_token}"
@@ -14,3 +16,8 @@ def post_new_user_kit(kit_body, auth_token):
     return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
                          json=kit_body,
                          headers=headers)
+
+
+# Función para obtener lista de usuarios existentes
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
